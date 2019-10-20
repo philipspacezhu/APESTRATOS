78e0f30ab4dba62ba971e1456128a3f11cfa7139
SGmagicsam <38333794+SGmagicsam@users.noreply.github.com>
2019-10-20 13:07:40 -0600

    embarrasing cut off point added to MVP

--- CSA_Website/static/js/map.js
+++ CSA_Website/static/js/map.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([48.5704, -81.3694], 15);
+const map = L.map('map').setView([48.5704, -81.3694], 11);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
